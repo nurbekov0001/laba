2a5a03f0639bcb40ff25ffecd3d42339ff4eb454
bekbolot <nurbekovbekbolot090501@gmail.com>
2021-03-08 16:00:15 +0600

    комит фикстуры dump

--- hello/webapp/models.py
+++ hello/webapp/models.py
@@ -10,7 +10,8 @@ class Product(models.Model):
     category = models.CharField(max_length=15, choices=CATEGORY_CHOICES, null=False, blank=False, default='other',
                                 verbose_name="категория товара")
     remainder = models.IntegerField(null=False, blank=False,  validators=[MinValueValidator(0)], verbose_name="Остаток")
-    price = models.DecimalField(null=False, blank=False, validators=[MinValueValidator(0)], max_digits=7, decimal_places=2, verbose_name="Стоимость")
+    price = models.DecimalField(null=False, blank=False, validators=[MinValueValidator(0)], max_digits=7,
+                                decimal_places=2, verbose_name="Стоимость")
 
     class Meta:
         db_table = 'Products'
@@ -19,7 +20,5 @@ class Product(models.Model):
 
     def __str__(self):
         return f'{self.id}. {self.name}:{self.description} {self.category} {self.remainder}{self.price}'
-
-
 
 # Create your models here.
